b044290276c655b12ffc73ebe21836cc1af8438a
Ahmad Dehnavi <ahmad.829@gmail.com>
2019-05-10 14:42:24 +0430

    add $intro service

--- src/services/intro/IntroService.ts
+++ src/services/intro/IntroService.ts
@@ -1,5 +1,26 @@
 import {PXBaseService} from '@shared';
+import {AsyncStorage} from 'react-native';
+
+export type InjectedIntroServiceProps = {
+    $intro:IntroService
+}
 
 export class IntroService extends PXBaseService {
+    public static readonly NAME = '$intro';
+    public static readonly IS_INTRO_ENABLED_KEY = '$intro/is-enabled';
+
+    async isIntroEnabled() {
+        try {
+            return await AsyncStorage.getItem(IntroService.IS_INTRO_ENABLED_KEY) === 'true';
+        } catch (e) {
+        }
+        // default value
+        return true;
+    }
+
+    setIntroEnabld(enabled: boolean) {
+        return AsyncStorage.setItem(IntroService.IS_INTRO_ENABLED_KEY, enabled ? 'true' : 'false');
+    }
 
 }
+
