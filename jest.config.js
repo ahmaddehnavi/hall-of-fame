a78ed6fdbe905c99081ce3693d688bebc136dc6b
Ahmad Dehnavi <ahmad.829@gmail.com>
2019-05-10 13:09:16 +0430

    fix test config

---
+++ jest.config.js
@@ -0,0 +1,25 @@
+const {defaults: tsjPreset} = require('ts-jest/presets');
+
+module.exports = {
+    ...tsjPreset,
+    preset: 'react-native',
+    setupFilesAfterEnv: ["<rootDir>__test__/setup.js"],
+    transform: {
+        ...tsjPreset.transform,
+        '\\.(ts|tsx)$': '<rootDir>/node_modules/react-native/jest/preprocessor.js',
+    },
+
+    // mock all raw files
+    moduleNameMapper: {
+        "\\.(jpg|ico|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$": "<rootDir>/__mocks__/fileMock.js"
+    },
+    // need parsed by babel
+    transformIgnorePatterns:[
+        "node_modules/(?!(jest-)?react-native|react-navigation)"
+    ],
+    globals: {
+        'ts-jest': {
+            babelConfig: true,
+        }
+    }
+};
