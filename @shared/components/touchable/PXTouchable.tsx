f171d6751d020652171d15044c8b4c655dbf4fe9
Ahmad Dehnavi <ahmad.829@gmail.com>
2019-05-10 14:38:12 +0430

    add touchable component

---
+++ @shared/components/touchable/PXTouchable.tsx
@@ -0,0 +1,47 @@
+import * as React from 'react';
+import {Platform, StyleSheet, TouchableNativeFeedback, TouchableOpacity, TouchableWithoutFeedback, TouchableWithoutFeedbackProps} from 'react-native';
+
+
+export type PXTouchableProps = TouchableWithoutFeedbackProps & {
+    circle?: boolean
+    withoutFeedback?: boolean
+}
+
+export function PXTouchable(props: PXTouchableProps) {
+
+    if (props.withoutFeedback) {
+        return (
+            <TouchableWithoutFeedback
+                {...this.props}
+            />
+        );
+    }
+    if (Platform.OS === 'android') {
+        if (Platform.Version >= 21) {
+            return (
+                <TouchableNativeFeedback
+                    {...this.props}
+                    background={
+                        props.circle ?
+                            TouchableNativeFeedback.SelectableBackgroundBorderless() :
+                            TouchableNativeFeedback.SelectableBackground()
+                    }
+                />
+            )
+        }
+        return (
+            <TouchableNativeFeedback
+                {...this.props}
+                background={undefined}
+            />
+        )
+    }
+
+    return (
+        <TouchableOpacity {...this.props}/>
+    )
+}
+
+const styles = StyleSheet.create({
+    row: {flexDirection: 'row'}
+});
