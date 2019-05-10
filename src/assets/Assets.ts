b10a39307acedd6c0ed961051926a227b65ad445
Ahmad Dehnavi <ahmad.829@gmail.com>
2019-05-10 21:52:37 +0430

    add gif animation

--- src/assets/Assets.ts
+++ src/assets/Assets.ts
@@ -1,6 +1,13 @@
 export const Assets = {
     sounds:{
         test:require('./sounds/test.wav')
+    },
+    images: {
+        gif_1: require('./images/1.gif'),
+        gif_2: require('./images/2.gif'),
+        gif_3: require('./images/3.gif'),
+        gif_4: require('./images/4.gif'),
+        gif_5: require('./images/5.gif'),
     }
 };
 
--- src/screens/welcome/WelcomeScreen.tsx
+++ src/screens/welcome/WelcomeScreen.tsx
@@ -1,49 +1,66 @@
-import {INavigationService, PXInject, PXInjectedNavigationServiceProps, MultiBackHandler, Row, Screen} from '@shared';
+import {DIInject, INavigationService, InjectedNavigationServiceProps, InjectedThemeServiceProps, MultiBackHandler, Row, Screen} from '@shared';
 import autobind from 'autobind-decorator';
+import {observer} from 'mobx-react';
 import React from 'react';
-import {BackHandler, Button, StyleSheet, Text, TextInput, View} from 'react-native';
+import {BackHandler, Button, Image, ImageSourcePropType, StyleSheet, TextInput, View} from 'react-native';
+import Assets from '../../assets/Assets';
 import {SoundUtil} from '../../utils/SoundUtil';
 import {FameListScreen} from '../fame-list/FameListScreen';
 
 type WelcomeScreenProps =
-    PXInjectedNavigationServiceProps
+    InjectedNavigationServiceProps &
+    InjectedThemeServiceProps
 
 
-@PXInject('$navigation')
-export class WelcomeScreen extends React.Component<WelcomeScreenProps> {
+type State = {
+    currentAnimationIndex: number
+}
+
+@DIInject('$navigation', '$theme')
+@observer
+export class WelcomeScreen extends React.Component<WelcomeScreenProps, State> {
     static readonly ROUTE_NAME = 'WelcomeScreen';
 
     static start(nav: INavigationService) {
         nav.navigate(this.ROUTE_NAME);
     }
 
+    state = {
+        currentAnimationIndex: 0
+    };
+
+    animations = [
+        Assets.images.gif_1,
+        Assets.images.gif_2,
+        Assets.images.gif_3,
+        Assets.images.gif_4,
+        Assets.images.gif_5,
+    ];
+    protected animationChangerIntervalId;
+
+    componentDidMount() {
+        this.animationChangerIntervalId = setInterval(this.showNextAnimation, 5000)
+    }
+
+    componentWillUnmount() {
+        clearInterval(this.animationChangerIntervalId);
+    }
+
+    @autobind
+    showNextAnimation() {
+        this.setState({
+            currentAnimationIndex: (this.state.currentAnimationIndex + 1) % this.animations.length
+        })
+    }
+
     render() {
         return (
-            <Screen style={styles.container}>
-                <Text>WelcomeScreen</Text>
-
-                <TextInput
-                    placeholder={'Enter a number'}
-                    style={styles.textInput}
-                />
-
-                <Row>
-                    <Button
-                        title={'Save'}
-                        onPress={this.handleSavePress}
-                    />
-                    <View style={{width:16}}/>
-                    <Button
-                        title={'Randomise'}
-                        onPress={this.handleRandomisePress}
-                    />
-                </Row>
-
-                <MultiBackHandler
-                    timeout={500}
-                    maxCount={2}
-                    onPress={this.handleBackPress}/>
-            </Screen>
+            <WelcomeComponent
+                onBackPress={this.handleBackPress}
+                onRandomisePress={this.handleRandomisePress}
+                oSavePress={this.handleSavePress}
+                animation={this.animations[this.state.currentAnimationIndex]}
+            />
         )
     }
 
@@ -62,7 +79,7 @@ export class WelcomeScreen extends React.Component<WelcomeScreenProps> {
         if (count === 1) {
             FameListScreen.start(this.props.$navigation)
         } else if (count === 2) {
-            SoundUtil.playTest()
+            SoundUtil.play(Assets.sounds.test)
                 .then(success => {
                     BackHandler.exitApp();
                 })
@@ -70,6 +87,47 @@ export class WelcomeScreen extends React.Component<WelcomeScreenProps> {
     }
 }
 
+type WelcomeComponentProps = {
+    oSavePress: () => void
+    onRandomisePress: () => void
+    onBackPress: (count: number) => void | boolean,
+    animation: ImageSourcePropType,
+}
+
+function WelcomeComponent(props: WelcomeComponentProps) {
+    // let styles = makeStyle(props.$theme);
+    return (
+        <Screen style={styles.container}>
+            <Image
+                style={{flex: 1, width: '90%'}}
+                resizeMode={'contain'}
+                source={props.animation}
+            />
+            <TextInput
+                placeholder={'Enter a number'}
+                style={styles.textInput}
+            />
+
+            <Row>
+                <Button
+                    title={'Save'}
+                    onPress={props.oSavePress}
+                />
+                <View style={{width: 16}}/>
+                <Button
+                    title={'Randomise'}
+                    onPress={props.onRandomisePress}
+                />
+            </Row>
+
+            <MultiBackHandler
+                timeout={500}
+                maxCount={2}
+                onPress={props.onBackPress}/>
+        </Screen>
+    )
+
+}
 
 const styles = StyleSheet.create({
     container: {
@@ -77,4 +135,5 @@ const styles = StyleSheet.create({
         justifyContent: 'center'
     },
     textInput: {}
+
 });
