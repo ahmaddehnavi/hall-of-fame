6c09b6abbc4b5fde83351b02eeb6e273e6408cac
Ahmad Dehnavi <ahmad.829@gmail.com>
2019-05-16 01:35:29 +0430

    add suggested gif

--- src/assets/Assets.ts
+++ src/assets/Assets.ts
@@ -3,11 +3,11 @@ export const Assets = {
         test:require('./sounds/test.wav')
     },
     images: {
-        gif_1: require('./images/1.gif'),
-        gif_2: require('./images/2.gif'),
-        gif_3: require('./images/3.gif'),
-        gif_4: require('./images/4.gif'),
-        gif_5: require('./images/5.gif'),
+        gif_1: require('./images/b1.gif'),
+        gif_2: require('./images/b2.gif'),
+        gif_3: require('./images/b3.gif'),
+        gif_4: require('./images/b4.gif'),
+        gif_5: require('./images/b5.gif'),
     }
 };
 
